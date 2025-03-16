6caa48014b0059b5ffedb8787403ee849d34ce23
kabelo-mastermind <0604749776kb@gmail.com>
2025-03-16 17:42:09 +0200

    allowed origin to be accesible by all

--- src/socket.js
+++ src/socket.js
@@ -5,12 +5,13 @@ let io;
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: [
-        'http://168.172.185.178:8081',  // First device IP
-        'http://10.100.9.10:8081',      // Second device IP
-        'http://localhost:8081',         // Localhost for local development
-        'https://your-app-name.onrender.com', // Render deployed URL
-      ],
+      // origin: [
+      //   'http://168.172.185.178:8081',  // First device IP
+      //   'http://10.100.9.10:8081',      // Second device IP
+      //   'http://localhost:8081',         // Localhost for local development
+      //   'https://your-app-name.onrender.com', // Render deployed URL
+      // ],
+      origin: "*",
       methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
     }
   });
